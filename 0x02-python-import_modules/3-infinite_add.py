28a2625640bd0d08f5557c9a6cfb95a893ecfaef
Willystone <willstown@icloud.com>
2021-11-20 18:42:52 +0300

    import modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
+from sys import argv
+
+
+def addArgs():
+    argc = len(argv)
+    if argc == 1:
+        return 0
+
     add = 0
-    for s in argv[1:]:
-        add += int(s)
-    print("{:d}".format(add))
+    index = 1
+    while index < argc:
+        add += int(argv[index])
+        index += 1
+
+    return add
+
+if __name__ == '__main__':
+    print(addArgs())
