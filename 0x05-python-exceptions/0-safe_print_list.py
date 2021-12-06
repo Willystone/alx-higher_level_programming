04881c20e854a890b6ae92fd1f5f147c9193e736
Willystone <willstown@icloud.com>
2021-12-07 02:37:31 +0300

    safe list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,16 @@
-  
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    idx = 0
+
     try:
-        new_list = my_list[:x]
-        print(''.join(str(s) for s in new_list))
-        count = 0
-        for elem in new_list:
-            count += 1
-        return count
-    except IndexError:
-        num = my_list[-1:]
-        counter = 0
-        for elem in my_list:
-            counter += 1
-        return counter
+        for i in my_list:
+            if idx < x:
+                print('{}'.format(my_list[idx]), end='')
+                idx += 1
+
+        print()
+    except TypeError:
+        pass
+    finally:
+        return idx  
+
